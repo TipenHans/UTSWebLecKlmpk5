c0c07c8dcec919aa46f162692999a11671f38773
ruci <163664122+Crestmo@users.noreply.github.com>
2024-10-24 16:59:57 +0700

    Add files via upload

--- view_user_detail.php
+++ view_user_detail.php
@@ -14,7 +14,7 @@ if (isset($_GET['user_id']) && !empty($_GET['user_id'])) {
     $stmt = $pdo->prepare("SELECT * FROM users WHERE id = :admin_id");
     $stmt->execute(['admin_id' => $admin_id]);
     $admin = $stmt->fetch();
-    
+
     $stmt = $pdo->prepare("SELECT full_name, email, phone_number, role, profile_picture FROM users WHERE id = :user_id");
     $stmt->execute(['user_id' => $user_id]);
     $user = $stmt->fetch();
@@ -38,7 +38,6 @@ if (isset($_GET['user_id']) && !empty($_GET['user_id'])) {
         <title>View User Detail</title>
         <link rel="stylesheet" href="style.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     </head>
     <body>
     <div class="header">
@@ -81,31 +80,16 @@ if (isset($_GET['user_id']) && !empty($_GET['user_id'])) {
                             </tr>
                         </tbody>
                     </table>
-                    <div class="d-flex justify-content-between">
-                        <a href="view_user.php" class="btn btn-secondary mt-3">Back to Users</a>
-                        <a href="#" class="btn btn-danger mt-3" id="deleteUserBtn">Delete User</a>
+                    <div>
+                    <a href="view_attended_event.php?user_id=<?php echo $user_id; ?>" class="btn btn-primary" class="d-flex justify-content-center">Event Information</a>
+                    </div>
+                    <div>
+                        <a href="view_user.php" class="btn btn-secondary mt-3" class="d-flex justify-content-start">Back to Users</a>
+                        <a href="delete_user.php?user_id=<?php echo $user_id; ?>" class="btn btn-danger mt-3" class="d-flex justify-content-end">Delete User</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
-    <script>
-        document.getElementById('deleteUserBtn').addEventListener('click', function() {
-            Swal.fire({
-                title: 'Are you sure?',
-                text: "You won't be able to revert this!",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Yes, delete it!'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    window.location.href = "delete_user.php?user_id=<?php echo $user_id; ?>";
-                }
-            });
-        });
-    </script>
     </body>
 </html>
