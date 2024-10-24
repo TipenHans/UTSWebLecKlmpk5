bae91a4c50fc967b299cdb0535840de9c71cbcff
TipenHans <167948766+TipenHans@users.noreply.github.com>
2024-10-24 20:22:57 +0700

    Update view_attended_event.php

--- view_attended_event.php
+++ view_attended_event.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION['user_id'])) {
     exit;
 }
 
-$user_id = $_SESSION['user_id'];
+$user_id = $_GET['user_id'];
 
 $stmt = $pdo->prepare("SELECT * FROM users WHERE id = :user_id");
 $stmt->execute(['user_id' => $user_id]);
